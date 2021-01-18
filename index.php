d32da50b177457ce4fa71feba2fe7e9c85a2ea68
Carlos Madriz <madrizcarlose@gmail.com>
2021-01-18 19:21:43 -0300

    commit 01181921

--- index.php
+++ index.php
@@ -6,91 +6,66 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="include/styles.css">
 <!-- Bootstrap --> 
  
  <title>Buenos Aires Top - Las mejores opciones donde comer, donde divertirse, seguros de vehículos.</title>
 
-
 </head>
-
 
 <body bgcolor="#afafaf">
 
-
-
-
 <br>
-
 <div class="container" >
     <div class="text-center">
         <div  style="width: 100%;">
             <p class="h1"  align="center">Guía de Buenos Aires</p>  
         </div>
     </div>
-    <br>
-    <div style="width: 100%;">
-        <p class="h4"></p>
- 
- 
-
-
-           <br> <br>
-  </div>
-<div class="container" >
-    <div class="row">
-		
+    <br><br><br>
+    
+	<div class="container" >
+    	<div class="row">
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
            <a href="menu_comidas.php"><p class="h2">Restaurantes</p>
 			<img src="images/app/menu/opt_comidas.jpeg" width="120" height="75" alt="Restaurantes" longdesc="Restaurantes" id="img_rest" /></a>
-		   <p class="h5">Carnes, pescado, pastas, pizzas, asiática..</p></font>
+		   <p class="text_menu">Carnes, pescado, pastas, pizzas, asiática..</p>
 		   <hr>
 		    <br>
         </div>
 		
-	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
-           <a href="listsites3.php?categoria=c020"><p class="h2">Bares</p>
-			<img src="images/app/menu/opt_bares.jpg" width="120" height="75" alt="Restaurantes" longdesc="Restaurantes" id="img_rest" /></a>
-		   <p><font color="black">Cervezas y tapas</p></font>
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+           <a href="listsites.php?categoria=c020"><p class="h2">Bares</p>
+			<img src="images/app/menu/opt_bares.jpg" width="120" height="75" alt="Bares" longdesc="Bares" id="img_bar" /></a>
+			<p class="text_menu">Cervezas y mas bebidas</p>
 		   <hr>  
 		    <br>
-	</div>
+		</div>
 		
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
-           <a href="listsites3.php?categoria=r000"><p class="h2">Actividades</p>
-			<img src="images/app/menu/opt_funny.jpg" width="120" height="75" alt="Restaurantes" longdesc="Restaurantes" id="img_rest" /></a>
-		   <p><font color="black">Parques, karting, tenis, golf,...</p></font>
+    	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+           <a href="listsites.php?categoria=r000"><p class="h2">Actividades</p>
+			<img src="images/app/menu/opt_funny.jpg" width="120" height="75" alt="Actividades" longdesc="Actividades" id="img_acti" /></a>
+			<p class="text_menu">Parques, karting, tenis, golf,...</p>
 		   <hr>  
 		   <br>
-	</div>
+		</div>
 	
-	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
            <a href="menu_diversion.php"><p class="h2">Servicios y productos</p>
-			<img src="images/app/menu/opt_servprof.jpeg" width="120" height="75" alt="Restaurantes" longdesc="Restaurantes" id="img_rest" /></a>
-		   <p><font color="black">Seguros, salud, estétcia, ...</p></font>
+			<img src="images/app/menu/opt_servprof.jpeg" width="120" height="75" alt="Servicios y productos" longdesc="Servicios y productos" id="img_serv" /></a>
+			<p class="text_menu">Seguros, salud, estética, ...</p>
 		   <hr>  
-	</div>
-	
-			
-	  </div> <!--div row-->
-	  </div>
-	  
-	 <br>
-    
-</div>
-
-
-
-
-</br></br>
+		</div>	
+		</div> <!--div row-->
+	</div> <!-- class="container" -->
+</div>  <!-- class="container" -->
+<br><br>
  
 <!--pie de pagina-->
 <?php 
-//include 'include/footer.inc';?>
+//include 'include/footer.inc';
 
-
-
-
-
+?>
 
 </body>
 </html>
--- listsites.php
+++ listsites.php
@@ -1,21 +1,36 @@
-﻿<?php 
+﻿
+
+<?php 
+
+//$lat  = $_SESSION["lat"];
+//$lon  = $_SESSION["lon"];
 $categoria = $_GET['categoria'];
-$str_sql="SELECT N.nomb_negocio,N.codi_negocio , N.desc_negocio ,N.valoracion ,N.telef_negocio ,N.imagen_negocio ,N.direccion_negocio,N.coord_negocio, N.coord2, N.webpage FROM t_negocios N JOIN tr_negocios_categorias R ON N.codi_negocio = R.codi_negocio WHERE R.id_categoria ='" .$categoria. "';";
+//$str_sql="SELECT N.nomb_negocio,N.codi_negocio , N.desc_negocio ,N.valoracion ,N.telef_negocio ,N.imagen_negocio ,N.direccion_negocio,N.coord_negocio, N.coord2, N.webpage FROM t_negocios N JOIN tr_negocios_categorias R ON N.codi_negocio = R.codi_negocio WHERE R.id_categoria ='" .$categoria. "' AND Activo=1;";
+
+$str_sql="SELECT N.* FROM t_negocios N JOIN tr_negocios_categorias R ON N.codi_negocio = R.codi_negocio WHERE R.id_categoria ='" .$categoria. "' AND Activo=1;";
+$str_sql2 = urlencode($str_sql);
+$str_sql3 = urldecode($str_sql2);
+
+$lat = -34.5737177;
+$lon = -58.4354472;
+$str_sql4="SELECT N.*, Round((( 3959 * acos( cos( radians('$lat') ) * cos( radians( N.lat ) ) * cos( radians( N.lon ) - radians('$lon') ) + sin( radians('$lat') ) * sin( radians( N.lat ) ) ) )*1.60934),1)  AS distance FROM t_negocios N JOIN tr_negocios_categorias R ON N.codi_negocio = R.codi_negocio WHERE R.id_categoria ='" .$categoria. "' AND Activo=1 Order By Distance ASC;";
+
+
 ?>
 
 <?php include 'include/db_conect.inc';?>
 
 
 <?php
-$result = $conn->query($str_sql);
+$result = $conn->query($str_sql4);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
    <?php include 'include/head.inc';?>
-   <link rel="stylesheet" href="css/mis_estilos.css">
-   
+   <link rel="stylesheet" href="../css/mis_estilos.css">
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
    
    
    
@@ -24,23 +39,76 @@ $result = $conn->query($str_sql);
 </head>
 
 <body>
+
+<?php// print $str_sql2; ?>
+
+<?php //print $str_sql3; ?>
+
 <div class="container-fluid" >
 
 <!--membrete general-->
-<?php include 'include/membrete.inc';?>
+<div class="container-fluid" style="background-color:#FBFCFC;" >
+	<div class="row">
+		<div class="col-sm-6 col-md-8 col-lg-8"> 
+		  	<img src="../imagenes/apps/logo01.png"  alt="" longdesc="" id="img_logo" height="42" width="82">
+			
+		</div>
+		<div class="col-sm-6 col-md-4 col-lg-4" align="right">
+			
+			<font size="1" face="arial" color=white><b>email:</b>buai@gmail.com</font>
+			</div>
+	</div>
+</div>
+<?php
+//include 'include/membrete_01.inc';?>
 
 	<div class="container-fluid" style="background-color:#fafafa;">
 		<br><br>
 		<div class="row gutter-10" >
 			
 				<div class="container-fluid" border=1 >
-					<div class="panel-group"   >
-						<?php
-    					while($row = $result->fetch_assoc()) {
+				
+				<form target="POPUPW" action="maps/map2a.php" method="post" 
+				onsubmit="POPUPW = window.open('about:blank','POPUPW',
+   'width=600,height=400');">
+					<input type="hidden" name="var_sql" value="<?php print($str_sql); ?>" />
+				<!--	<input id="xlat" type="input" name="lat"/>
+					<input id="xlon" type="input" name="lon"/>
+					<input type="submit" value="Ver mapa" />
+-->
+				</form>
+				<br>
+				
+				
+				<script>
+var xlat = document.getElementById("xlat");
+
+
+  if (navigator.geolocation) {
+	 // alert("xxx22");
+	  
+    navigator.geolocation.getCurrentPosition(showPosition);
+	 //alert("xxx999");
+} else { 
+ //alert("xxx3334");
+    xlat.innerHTML = "Geolocation is not supported by this browser.";
+  }
+
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude; 
+  document.getElementById('xlat').value = position.coords.latitude;
+}
+</script>
+		
+
+<?php
+while($row = $result->fetch_assoc()) {
 							
 									$max = "5"; // Numero total de imagenes
 $extension2 = ".png";// Definimos la extension, puede ser .jpg, gif, bmp, etc.
-$carpeta2 = "imagenes/apps/estrellas";//Carpeta con las imagenes
+$carpeta2 = "../imagenes/apps/estrellas";//Carpeta con las imagenes
 $start = "2";
 $random = mt_rand($start, $max);
 $image_name2 = $carpeta2  . $random . $extension2;
@@ -50,10 +118,15 @@ $image_name3= "img src='" . $image_name2 . "'  width=25,height=25 id='imagen' cl
 
 
 						?>
+						
+						 <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
+           
+			
+				<!--<div class="shadow p-3 mb-5 bg-white rounded">-->
 						<div class="panel panel-default"  >
 							<div class="panel-body ">
-							
-								<table border=0 width="100%">
+      
+           	<table border=0 width="100%">
 									<tr>
 									<td  valign="top" width="40" align="center">
 								
@@ -62,59 +135,58 @@ $image_name3= "img src='" . $image_name2 . "'  width=25,height=25 id='imagen' cl
 									print "<" . $foto;
 									?>
 									</p>
-									<?php
-									
-print "<" . $image_name2;
+								
 
-?> 
-									
+									<font face="helvetica"><small><?php print $row['distance']; ?>&nbsp;km</font>
+									<p><font face="helvetica"><small>
+									<?php
+									$coord = $row['coord2'];
+									$tira= "<a href=https://www.google.co.ve/maps/place/" .$coord . " onclick=" . "\"window.open(this.href, 'mywin', 'left=20,top=20,width=900,height=500,toolbar=1,resizable=0'); return false;" . "\" >Ver mapa</a>";
+									echo $tira;
+									?>
+									</small></font>
 									</td>
 									<td rowspan=2 width=10>&nbsp;&nbsp;&nbsp;</td>
 									<td class="align-top" >
 									<h5><font color="#086A87"><span id="titulo"> <?php print $row['nomb_negocio']; ?></span></h5></font>
 									<font face="helvetica" color="black">
-									<?php $rank = $row['valoracion']; ?>
+									<?php $rank = $row['valoracion']; ?>									
 									<div id="ocu_mayor_igual_800">
 										<?php print Substr($row['desc_negocio'], 0,50) . "..."; ?>
 									</div>
 									<!-- <div id="ocu_menor_igual_800"> -->
 										<?php print $row['desc_negocio']; ?>
 									<!--</div>-->
-									</font><p><a href="<?php print $row['webpage']; ?>" target="blank" >
-			<?php print $row['webpage']; ?></a><p>
+									</font><p><a href="<?php print $row['webpage']; ?>" target="blank" >Web page</a><p>
 									
-									<span class="glyphicon">&#xe182;</span>
+									<span class="glyphicon glyphicon-earphone"></span>
 									<?php print $row['telef_negocio']; ?>
 									<br>
 									</font>
 									<div>
 										<font face="helvetica"><small><b>Dir:&nbsp;</u></b>	<?php print $row['direccion_negocio']; ?></font></small>
-									</div>
-									<div id="ocu_mayor_igual_800">
-										<?php print Substr($row['direccion_negocio'], 0,50) . "..."; ?>
+																				
 									</div>
 									
 									
-	<font face="helvetica"><small>
-									<?php
-									//$coord = $row['coord_negocio'];
-									//$tira= "<a href=https://maps.google.co.ve/maps?q=" .$coord . " onclick=" . "\"window.open(this.href, 'mywin', 'left=20,top=20,width=900,height=500,toolbar=1,resizable=0'); return false;" . "\" >Ver mapa</a>";
-									$coord = $row['coord2'];
-									$tira= "<a href=https://www.google.co.ve/maps/place/" .$coord . " onclick=" . "\"window.open(this.href, 'mywin', 'left=20,top=20,width=900,height=500,toolbar=1,resizable=0'); return false;" . "\" >Ver mapa</a>";
-									echo $tira;
-									?>
-									</small></font>
+	
 									
 									</td>
 									
 									</tr>
 									
 								</table>
-								
-									
-									
-							</div>
+                   
+                   </div>
+						<!--</div>-->
+						
 						</div>
+                   
+                    </div>
+						
+						
+						
+						
 							<?php
 							}
 							$conn->close();
@@ -126,7 +198,8 @@ print "<" . $image_name2;
 	</div>
 
 <!--pie de pagina-->
-<?php include 'include/footer.inc';?> 
+<?php 
+//include 'include/footer.inc';?> 
 
 </div>
 </body>
