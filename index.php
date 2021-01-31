db20de68a74c57d761e280c74da1ef7249007225
Carlos Madriz <madrizcarlose@gmail.com>
2021-01-31 17:50:23 -0300

    commit xyz

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php include 'include/head.inc';?>
-<!-- Bootstrap
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
- Bootstrap --> 
-<link rel="stylesheet" href="include/styles.css">
- <title>Buenos Aires Top - Las mejores opciones donde comer, donde divertirse, seguros de vehículos.</title>
+	<?php include 'include/head.inc';?>
+	<?php include 'include/mixpanel.inc';?>
+	<?php include 'include/mixp_functions.inc';?>
 
+	
+	<!-- Bootstrap
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	 Bootstrap --> 
+	<link rel="stylesheet" href="include/styles.css">
+ 	<title>Buenos Aires Top - Las mejores opciones donde comer, donde divertirse, seguros de vehículos.</title>
 </head>
 
 <body>
@@ -37,7 +40,7 @@
     <div class="row">
 		<!-- opcion Restaurantes-->
 		<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-           <a href="restaurants.php"><p class="h1">Restaurantes</p>
+		    <a href="#" onclick="openPage('restaurants.php');"><p class="h1">Restaurantes</p>
 			<img src="images/app/menu/opt_comidas.jpeg" width="120" height="75" alt="Restaurantes" longdesc="Restaurantes" id="img_rest" /></a>
 		   <p class="text_menu">Carnes, pescado, pastas, pizzas, asiática..</p>
 		   <hr>
@@ -45,7 +48,8 @@
         </div>
 		<!-- opcion Bares-->
 		<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-           <a href="listsites.php?categoria=c020"><p class="h1">Bares</p>
+		  <!-- <a href="listsites.php?categoria=c020"><p class="h1">Bares</p> -->
+		   <a href="#" onclick="showList('c020');"><p class="h1">Bares</p>
 			<img src="images/app/menu/opt_bares.jpg" width="120" height="75" alt="Bares" longdesc="Bares" id="img_bar" /></a>
 			<p class="text_menu">Cervezas y mas bebidas</p>
 		   <hr>  
@@ -53,7 +57,8 @@
 		</div>
 		<!-- opcion Actividades-->
     	<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-           <a href="listsites.php?categoria=r000"><p class="h1">Actividades</p>
+		  <!-- <a href="listsites.php?categoria=r000"><p class="h1">Actividades</p> -->
+		   <a href="#" onclick="showList('r000');"><p class="h1">Actividades</p>
 			<img src="images/app/menu/opt_funny.jpg" width="120" height="75" alt="Actividades" longdesc="Actividades" id="img_acti" /></a>
 			<p class="text_menu">Parques, karting, tenis, golf,...</p>
 		   <hr>  
@@ -61,13 +66,16 @@
 		</div>
 		<!-- opcion Servicios-->
 		<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-           <a href="menu_diversion.php"><p class="h1">Serv. y productos</p>
+		 <!--   <a href="services.php"><p class="h1">Serv. y productos</p> -->
+		   <a href="#" onclick="openPage('services.php');"><p class="h1">Serv. y productos</p>
 			<img src="images/app/menu/opt_servprof.jpeg" width="120" height="75" alt="Servicios y productos" longdesc="Servicios y productos" id="img_serv" /></a>
 			<p class="text_menu">Seguros, salud, estética, ...</p>
 		   <hr>  
 		</div>	
-	</div> <!--div row-->
-	</div> 
+
+		
+	</div> <!--div row -->
+	</div>  <!--div template -->
 </div> <!-- class="container" -->
   
 <br><br><br><br><br><br><br><br>
--- listsites.php
+++ listsites.php
@@ -13,48 +13,89 @@ $categoria = $_GET['categoria'];
 $lat = -34.5737177;
 $lon = -58.4354472;
 
-//$str_sql4="SELECT N.*, Round((( 3959 * acos( cos( radians('$lat') ) * cos( radians( N.lat ) ) * cos( radians( N.lon ) - radians('$lon') ) + sin( radians('$lat') ) * sin( radians( N.lat ) ) ) )*1.60934),1)  AS distance FROM t_negocios N JOIN tr_negocios_categorias R ON N.codi_negocio = R.codi_negocio WHERE R.id_categoria ='" .$categoria. "' AND Activo=1 Order By Distance ASC;";
+
+//--------------------------------------
+$url = "http://localhost/buenosairestop/get_sites.php?categoria=" . $_GET['categoria'];
+$data = file_get_contents($url);
 
 
-?>
+//$url = "http://localhost/buenosairestop/get_sites.php";
 
-<?php 
-//include 'include/db_conect.inc';
-require "get_sites.php";
-?>
+//$ch = curl_init();
 
-<?php
-//$result = $conn->query($str_sql4);
+    //curl_setopt($ch, CURLOPT_HEADER, 0);
+    //curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    //curl_setopt($ch, CURLOPT_URL, $url);
+   // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    //$data = curl_exec($ch);
+    //curl_close($ch);
+
+   
+
+//$data = file_get_contents("http://localhost/buenosairestop/get_sites.php");
+$products = json_decode($data,true);
+
+//$result = file_get_contents($url);
+//$array = json_decode($result, true);
+//var_dump($products);
+ 
+//$products = $data;
+//-- 
+//-- foreach ($products as $product) {
+ //--    echo '<pre>';
+ //--    print_r($product);
+  //--   echo '</pre>';
+//-- }
+//-----------------------------------------
+//require "get_sites.php";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style type="text/css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style type="text/css">
     .container.custom-container {
       padding: 0 20px;
     }
-</style>
+  </style>
    <?php include 'include/head.inc';?>
    <!--
    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 -->
    <title>Buenos Aires - Mejores sitios</title>
 
-<script>
-function selectText(containerid) {
-    if (document.selection) { // IE
-        var range = document.body.createTextRange();
-        range.moveToElementText(document.getElementById(containerid));
-        range.select();
-    } else if (window.getSelection) {
-        var range = document.createRange();
-        range.selectNode(document.getElementById(containerid));
-        window.getSelection().removeAllRanges();
-        window.getSelection().addRange(range);
+  <script>
+    function selectText(containerid) {
+       if (document.selection) { // IE
+            var range = document.body.createTextRange();
+            range.moveToElementText(document.getElementById(containerid));
+            range.select();
+        } else if (window.getSelection) {
+            var range = document.createRange();
+            range.selectNode(document.getElementById(containerid));
+            window.getSelection().removeAllRanges();
+            window.getSelection().addRange(range);
+        }
     }
-}
+  </script>
+  <script>
+    var xlat = document.getElementById("xlat");
+    if (navigator.geolocation) {
+	      // alert("xxx22");
+        navigator.geolocation.getCurrentPosition(showPosition);
+	      //alert("xxx999");
+    } else { 
+        //alert("xxx3334");
+        xlat.innerHTML = "Geolocation is not supported by this browser.";
+      }
+
+    function showPosition(position) {
+        x.innerHTML = "Latitude: " + position.coords.latitude + 
+      "<br>Longitude: " + position.coords.longitude; 
+      document.getElementById('xlat').value = position.coords.latitude;
+    }
 </script>
 </head>
 
@@ -83,59 +124,34 @@ function selectText(containerid) {
 	<div class="container-fluid" style="background-color:#fafafa;">
 		<br><br>
 		<div class="row gutter-30" >
-			<!--
-			<div class="container-fluid" border=1 >
--->
-				<form target="POPUPW" action="maps/map2a.php" method="post" 
+					<form target="POPUPW" action="maps/map2a.php" method="post" 
 				onsubmit="POPUPW = window.open('about:blank','POPUPW',
    'width=600,height=400');">
-				
-				<!--	<input id="xlat" type="input" name="lat"/>
-					<input id="xlon" type="input" name="lon"/>
-					<input type="submit" value="Ver mapa" />
--->
-				</form>
+					</form>
 				<br>	
-				
-<script>
-var xlat = document.getElementById("xlat");
- if (navigator.geolocation) {
-	 // alert("xxx22");
-    navigator.geolocation.getCurrentPosition(showPosition);
-	 //alert("xxx999");
-} else { 
- //alert("xxx3334");
-    xlat.innerHTML = "Geolocation is not supported by this browser.";
-  }
-
-
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude; 
-  document.getElementById('xlat').value = position.coords.latitude;
-}
-</script>
-
-
 <?php
-//while($row = $result->fetch_assoc()) {
 
-	while($row = $result2->fetch_assoc()) {
-							
 $max = "5"; // Numero total de imagenes
 $extension2 = ".png";// Definimos la extension, puede ser .jpg, gif, bmp, etc.
 $carpeta2 = "../imagenes/apps/estrellas";//Carpeta con las imagenes
 $start = "2";
-$random = mt_rand($start, $max);
-$image_name2 = $carpeta2  . $random . $extension2;
-$image_name2= "img src='" . $image_name2 . "'  width=52,height=28 id='imagen' class='imagen'/> ";
-$image_name3= "img src='" . $image_name2 . "'  width=25,height=25 id='imagen' class='imagen'/> ";
-?>
 
-<?php
-				$foto = "images/sites/" . $row['imagen_negocio'] ;
-			
-				?>
+//$row = $products;
+//var_dump($products);
+$longitud = count($products);
+
+//while($row = $products->fetch_assoc()) {
+  //foreach ($products as $row) {
+    for($i=0; $i<$longitud; $i++) {
+  
+      $row = $products[$i];
+     // var_dump($row);
+  $random = mt_rand($start, $max);
+  $image_name2 = $carpeta2  . $random . $extension2;
+  $image_name2= "img src='" . $image_name2 . "'  width=52,height=28 id='imagen' class='imagen'/> ";
+  $image_name3= "img src='" . $image_name2 . "'  width=25,height=25 id='imagen' class='imagen'/> ";
+	$foto = "images/sites/" . $row['imagen_negocio'] ;
+?>
 <div class="col-xs-6 col-sm-4 col-md-4 col-lg-3"  >	
 <div class="shadow p-0 mb-5 bg-white rounded">
     <img class="card-img-top" src="<?php echo $foto;?>" alt="Card image cap" height='240'>
@@ -144,7 +160,6 @@ $image_name3= "img src='" . $image_name2 . "'  width=25,height=25 id='imagen' cl
         <div id="site2<?php echo $row['codi_negocio']; ?>">
 				    <h4><font color="#086A87"><?php print $row['nomb_negocio']; ?></h4></font>
         </div>
-        
         </h5>
        <p>
 		    <span class="glyphicon glyphicon-earphone"></span>
@@ -153,49 +168,39 @@ $image_name3= "img src='" . $image_name2 . "'  width=25,height=25 id='imagen' cl
         </span>
         <p>
         <span class="glyphicon glyphicon-map-marker"></span>
-            <?php print $row['direccion_negocio'];?>
-     </div>
+        <?php print $row['direccion_negocio'];?>
+    </div> <!-- class="card-body" -->
     <ul class="list-group list-group-flush">
-    
-    <li class="list-group-item"  style="background-color:#fafafa;">
-   
-        <div style="width: 50%; float:left">
-        <a href="<?php print $row['webpage']; ?>" target="blank" >Web page</a>
+        <li class="list-group-item"  style="background-color:#fafafa;">
+           <div style="width: 50%; float:left">
+              <a href="<?php print $row['webpage']; ?>" target="blank" >Web page</a>
+          </div>
+          <div style="width: 50%; float:right;" align="right">
+            <?php 
+            $coord = $row['coord2'];
+            $tira= "<a href=https://www.google.co.ve/maps/place/" .$coord . " onclick=" . "\"window.open(this.href, 'mywin', 'left=200,top=20,width=900,height=500,toolbar=1,resizable=0'); return false;" . "\" align='right'>Ver mapa</a>";
+            echo $tira;
+            ?>
+            <font face="helvetica"><small><?php print $row['distance']; ?>&nbsp;km</font></small>
+          </div>
+       </li>
+    </ul>
 </div>
-
-<div style="width: 50%; float:right;" align="right">
-<?php 
-           $coord = $row['coord2'];
-           $tira= "<a href=https://www.google.co.ve/maps/place/" .$coord . " onclick=" . "\"window.open(this.href, 'mywin', 'left=200,top=20,width=900,height=500,toolbar=1,resizable=0'); return false;" . "\" align='right'>Ver mapa</a>";
-               echo $tira;
-               ?>
-               <font face="helvetica"><small><?php print $row['distance']; ?>&nbsp;km</font>
-				</small>
-</div>
-    </li>
-   
-  </ul>
-  </div>
 </div>	
 
-
-
 <?php
-//fin de bucle
-}
-//se cierra la conexioón
-//$conn->close();
+}  //fin de bucle
 ?>
 			
+</div> <!-- row -->
 </div>
-</div>
-</div>
+</div> <!-- template -->
 </div>
 
 <br><br><br><br>
 <!--pie de pagina-->
 <?php 
-require "include/db_conect_close.inc";
+//require "include/db_conect_close.inc";
 include 'include/footer.inc';
 ?> 
 
