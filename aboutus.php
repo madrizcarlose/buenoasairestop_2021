fff0f9711d0e43de0c4d3f2a989e7493037535f8
Carlos Madriz <madrizcarlose@gmail.com>
2021-02-01 17:24:27 -0300

    commit 02011724

---
+++ aboutus.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php include 'include/head.inc';?>
+</head>
+
+
+<body>
+<!-- logo y titulo-->
+<div class="container">
+<div class="template">
+	<div class="row">
+		<div class="col-sm-4 col-md-4 col-lg-4" align="left"> 
+			<a href="index.php"><img src="images/app/utils/logo2.jpg"  alt="" longdesc="" id="img_logo"  height="62" width="130">
+			</a>
+		</div>
+		<div class="col-sm-8 col-md-8 col-lg-8" align="left"> 
+			<p class="h2"  align="left" color=#086A87>Buenos Aires Top</p> 
+		</div>
+	</div>
+</div> <!-- template-->
+</div> <!-- container-->
+
+<div class="container" >
+<!--membrete general-->
+<?php include 'include/membrete_01.inc';?>
+
+</br></br>
+
+<div class="row">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-9" align="center">
+<h2><font color=#086A87>Acerca de Nosotros...</h3>
+<br><br>
+<font color=#0B2161 align="justify"><h4>Somos un página web creada en el año 2019 con el objetivo de promover y dar a conocer las diferentes opciones que ofrece la ciudas de Buenos Aires en hospedaje, restaurantes, donde comprar, sitios recreacionales, etc. Adicionalmente servimos como una plataforma de publicación gratis de avisos clasificados asociados a oferta de artículos o servicios.
+<br><br>
+
+<br><br>
+
+Nuestro objetivo es ayudar a los ciudadanos en la búsqueda de información y apoyar a los emprendimientos en la divulgación de sus sitios.
+
+ <br><br>
+Sientase libre de contactarnos, hacer preguntas y enviar sugerencias y comentarios relacionados con nuestra página, aseguramos poder atenderlo y responderle oportunamente. </p>
+<br>
+<br>Un cordial saludo.
+
+</div> 
+<div class="col-xs-4 col-sm-4 col-md-6 col-lg-3" align="center" id="ocu_menor_igual_800">
+<img src="images/app/utils/about02.jpg">
+</div> 
+</div> 
+ <br>
+ <?php 
+include 'include/footer.inc';
+?>
+</body>
---
+++ politica_privacidad.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php include 'include/head.inc';?>
+</head>
+
+
+<body>
+
+<!-- logo y titulo-->
+<div class="container">
+<div class="template">
+	<div class="row">
+		<div class="col-sm-4 col-md-4 col-lg-4" align="left"> 
+			<a href="index.php"><img src="images/app/utils/logo2.jpg"  alt="" longdesc="" id="img_logo"  height="62" width="130">
+			</a>
+		</div>
+		<div class="col-sm-8 col-md-8 col-lg-8" align="left"> 
+			<p class="h2"  align="left" color=#086A87>Buenos Aires Top</p> 
+		</div>
+	</div>
+</div> <!-- template-->
+</div> <!-- container-->
+
+<div class="container" >
+<!--membrete general-->
+<!--membrete general-->
+<?php include 'include/membrete_01.inc';?>
+</br></br>
+
+<div class="row">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" align="center">
+<h2><font color=#086A87>Política de Privacidad</h2>
+<br>
+<font color=#0B2161 align="justify">
+
+<blockquote>
+
+<font size=2><p>La privacidad de nuestros visitantes es importante, por lo que te ponemos al tanto de
+las debidas condiciones de uso en este sitio.</p>
+<p>La utilización de este sitio implica su aceptación plena y sin reservas a
+todas y cada una de las disposiciones incluidas en este Aviso Legal, por
+lo que si usted no está de acuerdo con cualquiera de las condiciones
+aquí establecidas, no deberá usar u/o acceder a este sitio. </p></font>
+<br>
+
+
+<h3>Uso de cookies</h3>
+<font size=2><p>Empleamos cookies y otras tecnologías para estudiar y medir la cantidad de tráfico en nuestro sitio web como la mayoría de sitios webs lo hacen sino es que 
+todos, de forma que sus visitas sean aún más gratificantes, y para saber que tanto ha sido del agrado el sitio y en que podemos mejorarlo. 
+Nuestro sitio www.guiaparaguana.com también puede utilizar cookies para registrar sus visitas a nuestras diferentes páginas.</p></font>
+<br>
+<h3><p>Publicidad del sitio</p></h3>
+<font size=2><p style="text-align: justify;">En algunos casos, en este sitio compartimos información sobre los visitantes de forma anónima o agregada con terceros como puedan ser anunciantes, patrocinadores o auditores con el único fin de mejorar nuestros servicios.</p>
+
+<p style="text-align: justify;">Todas estas tareas de procesamiento serán reguladas según las normas legales y se respetarán todos sus derechos en materia de protección de datos conforme a la regulación vigente. Este sitio también puede albergar publicidad propia, de afiliados, o de redes publicitarias y siempre de acuerdo a la Ley de Servicios de la Sociedad de la Información y de Comercio Electrónico (LSSI) .</p>
+<p style="text-align: justify;">Esta publicidad se muestra mediante servidores publicitarios que utilizan cookies para mostrar contenidos publicitarios afines a los usuarios.</p>
+<p style="text-align: justify;">Cada uno de estos servidores publicitarios dispone de su propia política de privacidad, que puede ser consultada en sus propias páginas web. Actualmente este sitio muestra publicidad de:&nbsp;&nbsp;<strong>Google Adsense</strong> – https://www.google.com/adsense?hl=es</p>
+<p style="text-align: justify;">En el caso específico de Google Adsense, esta compañía utiliza una publicidad basada en intereses que permite a los anunciantes mostrar anuncios basados en las interacciones previas de los usuarios, como las visitas al sitio web del anunciante, así como llegar al usuario según sus intereses (por ejemplo, “amantes del deporte”). Para crear las categorías de intereses, detectan los tipos de páginas web que los usuarios visitan a través de la red de contenido de Google.</p>
+<p style="text-align: justify;">Por ejemplo, si un usuario visita un número determinado de páginas de deportes, lo añaden a la categoría de interés “amantes del deporte”.Google utiliza para ello la cookie de DoubleClick DART (solo disponible en inglés) en los anuncios publicados en sitios web de editores que muestran anuncios de AdSense para contenido, como es este caso. Cuando un usuario final accede a un sitio web del editor de AdSense y ve un anuncio o hace clic en él, es posible que se introduzca una cookie en su navegador. Los datos que se recopilan a partir de estas cookies se utilizan para ayudar a los editores de AdSense a publicar y administrar mejor los anuncios de sus sitios y de toda la Web, por ello tenga en cuenta que:</p>
+<p style="text-align: justify;">Google, como proveedor asociado, utiliza cookies para publicar anuncios en este sitio.</p>
+<p style="text-align: justify;">Google utiliza empresas publicitarias asociadas para publicar anuncios cuando visita nuestro sitio web. Es posible que estas empresas usen la información que obtienen de sus visitas a este y otros sitios web (sin incluir su nombre, dirección, dirección de correo electrónico o número de teléfono) para ofrecerle anuncios sobre productos y servicios que le resulten de interés.</p>
+<p style="text-align: justify;">Si quiere ver publicidad relacionada con sus intereses personales, puede editar las categorías de intereses que se basan en sitios web que ha visitado recientemente.</p>
+<p style="text-align: justify;">Los usuarios pueden inhabilitar el uso de la cookie de DART a través del anuncio de Google Adsense y accediendo a la política de privacidad de la red de contenido. Para guardar de forma definitiva sus preferencias de inhabilitación puede instalar este complemento para el navegador que le permitirá mantener su selección de inhabilitación, incluso en caso de borrar todas las cookies.</p>
+<br><h3>Cookies – Remarketing</h3>
+<font size=2><p style="text-align: justify;">Para saber más sobre el control de su experiencia en línea visite la página Network Advertising Initiative (NAI) (sólo disponible en inglés).</p>
+<p>Nuestra página utiliza unos ficheros llamados “cookies”, que permiten identificarlo cuando regrese a nuestra página y poder darle un trato más personalizado y relevante para usted.</p>
+<p>Además en cualquier momento usamos la función de anuncios de remarketing, donde nuestra plataforma publicitaria de Google AdWords nos permite identificarlo a través de su IP para poderle mostrar anuncios de remarketing sin importar en que sitio web este navegando, conocida como publicidad de Display.</p>
+<p>La protección de la privacidad es una de nuestras prioridades, por lo que ofrecemos una completa transparencia sobre la información que recopilamos y sobre cómo vamos a utilizar esa información. En ningún caso recopilamos, vendemos ni compartimos información personal identificativa de los usuarios procedente de cookies de publicación de anuncios sin su consentimiento.</p>
+<p>El usuario tiene la posibilidad de configurar su navegador para ser avisado de la recepción de cookies y para impedir su instalación en su equipo. Asimismo, el usuario puede revisar en su navegador qué cookies tiene instaladas y cuál es la fecha de caducidad de las mismas, así como eliminarlas. Por favor, consulte las instrucciones y manuales de su navegador para ampliar esta información.</p>
+<p>Si prefieres que no aparezcan anuncios basados en tus intereses en los navegadores web que utilizas (anuncios de remarketing), puedes hacer clic en cualquier momento en el botón «Inhabilitar» del <a href="https://www.google.com/ads/preferences/?hl=es" target="_blank">Administrador de preferencias de anuncios</a>. Cuando accedes a Internet a través de un navegador web, Google también te ofrece una serie de opciones para <a href="https://www.google.com/settings/ads/onweb/?sig=ACi0TCi95QAkOO_bcHYTE8vkrkaH-YcdRWfPMJ8OPF0q0U_39ULhxSuVyIpLEzXeZ84U35palCS9HFCVkhN0fsqpDGz-RM09P2nIuLZsDlMb8mQg-PYhz6QBNJYrnqgvPzJ7RYJwR-kp5QSHMWyPPMsEE_VmBroOr1kIc2wqsFpGHbP6uLAhbQA&amp;hl=es#display_optout" target="_blank">guardar permanentemente la configuración de inhabilitación</a> en tu navegador. Una vez que hayas inhabilitado la opción, Google no volverá a recopilar información sobre categorías de intereses y dejarás de recibir anuncios basados en tus intereses a través de Google al acceder a Internet mediante un navegador web.</p>
+<p>Podrás inhabilitar el uso de cookies por parte de Google AdWords y Analytics al visitar la <a href="http://www.google.com/settings/ads" target="_blank">Configuración de anuncios</a> de Google que tienes en la actualidad. También podrás inhabilitar el uso de cookies de proveedores externos de la <a href="http://www.networkadvertising.org/managing/opt_out.asp" target="_blank">página de inhabilitación de Network Advertising Initiative</a> (en inglés).</p>
+<p>Si deseas mayor información al respecto te invito a conocer el <a href="https://tools.google.com/dlpage/gaoptout/">complemento de inhabilitación para navegadores de Google Analytics</a>.</p>
+<p>Las Cookies de Analytics de Google Inc. permiten realizar análisis del Sitio Web recopilando métricas de la navegación del usuario tales como duración de la visita al sitio web, páginas visitadas, procedencia geográfica del usuario, entre otras métricas.</p>
+<p>No podemos asumir ninguna responsabilidad  en caso de que esta página, o nuestro sistema de almacenamiento de datos sea penetrada por “hackers”, lo que pueda causar un uso indebido de su información. Sin embargo, le garantizamos que siempre haremos lo posible para protege nuestro sistema, con la tecnología que se encuentra a nuestro alcance, para impedir este tipo de violaciones.</p>
+<br>
+<h3>Política de Cookies</h3>
+<font size=2><p>¿QUÉ SON LAS COOKIES?</p>
+<font size=2><p>Una cookie es un fichero que se descarga en tu ordenador al acceder a determinadas páginas web. Las cookies permiten a una página web, entre otras cosas, almacenar y recuperar información sobre los hábitos de navegación de un usuario o de su equipo y, dependiendo de la información que contengan y de la forma en que utilice su equipo, pueden utilizarse para reconocer al usuario.</p>
+<p>TIPOS DE COOKIES UTILIZADAS EN EL PRESENTE SITIO WEB:</p>
+<p>-Cookies analíticas: Las cookies analíticas recogen información para evaluar el uso que haces de la Web y la actividad general de la misma. La analítica de la Web es la medida, recolección, análisis y creación de informes de los datos de Internet con el propósito de entender y optimizar el uso de sitio Web.<br />
+Google Analytics: <a class="external" href="http://www.google.com/analytics/learn/privacy.html?rd=2" target="_blank">http://www.google.com/analytics/learn/privacy.html?rd=2</a></p>
+
+<p>-Cookies de publicidad y comportamentales: Son aquéllas que nos permiten gestionar de la forma más eficaz posible la oferta de los espacios publicitarios que hay en la página web, adecuando el contenido del anuncio al contenido del servicio solicitado o al uso que realice de nuestra página web. Para ello podemos analizar tus hábitos de navegación en Internet y podemos mostrarte publicidad relacionada con tu perfil de navegación.<br />
+Google DFP y Adsense: <a class="external" href="http://www.google.es/policies/technologies/ads/" target="_blank">http://www.google.es/policies/technologies/ads/</a></p>
+<p>-Cookies de afiliados: Las cookies de afiliados permiten hacer un seguimiento de las visitas procedentes de otras Webs, con las que este sitio tiene un contrato de afiliación.</p>
+<p>-Cookies funcionales: Las cookies funcionales son las estrictamente necesarias para proporcionar los servicios solicitados por los usuarios en este sitio Web.</p>
+<p>CÓMO GESTIONAR LAS COOKIES EN TU EQUIPO:</p>
+<p>La mayor parte de los Navegadores soportan el uso de Cookies. Sin embargo, como usuario puedes elegir si aceptas las Cookies o no. Las opciones más comunes que ofrecen los Navegadores son:<br />
+-No aceptar Cookies nunca.<br />
+-El Navegador pregunta al usuario si se debe aceptar cada cookie individualmente.<br />
+-Aceptar Cookies siempre.</p>
+<p>Tu Navegador también puede incluir la posibilidad de especificar mejor qué Cookies deben ser aceptadas y cuáles no. Además, también tienes la posibilidad de ver y borrar Cookies individualmente. Si desconoces cómo gestionar sus Cookies y la política de privacidad de tu navegador web, puedes encontrarlo en la ayuda del navegador.</p>
+<p>MANEJO DE COOKIES EN LOS DIFERENTES NAVEGADORES:</p>
+<p>Te recomendamos que consultes la ayuda de tu navegador. O accedas a las páginas web de ayuda de los principales Navegadores:<br />
+-Firefox: <a class="external" href="http://support.mozilla.org/es/home" target="_blank">http://support.mozilla.org/es/home</a><br />
+-Internet Explorer: <a class="external" href="http://support.microsoft.com/ph/807/es-es" target="_blank">http://support.microsoft.com/ph/807/es-es</a><br />
+-Safari: <a class="external" href="http://support.apple.com/kb/HT1677?viewlocale=es_ES" target="_blank">http://support.apple.com/kb/HT1677?viewlocale=es_ES</a><br />
+-Opera: <a class="external" href="http://help.opera.com/Windows/8.54/es-ES/index.html" target="_blank">http://help.opera.com/Windows/8.54/es-ES/index.html</a><br />
+-Google Chrome: <a class="external" href="https://support.google.com/chrome/answer/95647?hl=es" target="_blank">https://support.google.com/chrome/answer/95647?hl=es</a></p>
+
+
+
+<h3><br><p><strong>Contacto</strong></p></h3>
+<font size=2><p>Si tiene preguntas o cuestiones sobre esta Política, no dude en
+contactarse en cualquier momento a través del formulario de contacto
+disponible en el sitio o por medio del correo electrónico
+vamosba@gmail.com</p>
+
+<br>Un cordial saludo.
+</blockquote>
+
+</div>
+		</div>
+	
+
+
+ <br>
+ 
+ 
+	
+
+	
+
+  
+
+
+<!--pie de pagina-->
+<?php include 'include/footer.inc';?> 
+
+</div>   
+</body>
+</html>
