d7f42e4729b8e04ee827e239d1ff70d7a7a765ec
Carlos Madriz <madrizcarlose@gmail.com>
2021-02-01 19:28:51 -0300

    commit 02011928

--- index.php
+++ index.php
@@ -82,13 +82,31 @@
 		   <a href="#" onclick="openPage('services.php');"><p class="h1">Serv. y productos</p>
 			<img src="images/app/menu/opt_servprof.jpeg" width="120" height="75" alt="Servicios y productos" longdesc="Servicios y productos" id="img_serv" /></a>
 			<p class="text_menu">Seguros, salud, estética, ...</p>
-		   <hr>  
+		   <hr> 
+		   
+		   
 		</div>	
 
 		
 	</div> <!--div row -->
 	</div>  <!--div template -->
 </div> <!-- class="container" -->
+
+<!-- GetButton.io widget -->
+<script type="text/javascript">
+(function () {
+var options = {
+whatsapp: "+5491127835577", // WhatsApp number
+call_to_action: "Envíanos un mensaje", // Call to action
+position: "left", // Position may be 'right' or 'left'
+};
+var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+})();
+</script>
+<!-- /GetButton.io widget -->
   
 <br><br><br><br><br><br><br><br>
 <!--pie de pagina-->
