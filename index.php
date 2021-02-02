0f3f87a7e489abf158c39e8b3d102ac057145b2c
Carlos Madriz <madrizcarlose@gmail.com>
2021-02-02 18:55:18 -0300

    commit 02021855

--- index.php
+++ index.php
@@ -67,12 +67,13 @@ else{
 
         ._img{
             overflow: hidden;
-            width: 50px;
-            height: 50px;
+            width: 30px;
+            height: 30px;
             margin: 0 auto;
             border-radius: 50%;
             box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
 			display: inline-block;
+			vertical-align: top;
         }
         ._img > img{
             width: 50px;
