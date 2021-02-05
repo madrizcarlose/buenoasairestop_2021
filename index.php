8e945f5f8655c9f97436ddd5aa667b4dc0b29dc0
Carlos Madriz <madrizcarlose@gmail.com>
2021-02-05 08:57:31 -0300

    commit 02050857

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ require 'db_connection.php';
 //}
 
 $id = $_SESSION['login_id'];
-
 $get_user = mysqli_query($db_connection, "SELECT * FROM `users` WHERE `google_id`='$id'");
 
 if(mysqli_num_rows($get_user) > 0){
@@ -23,17 +22,9 @@ else{
 <html lang="en">
 <head>
 
-
-
-
-
-
-
-
 	<?php include 'include/head.inc';?>
 	<?php include 'include/mixpanel.inc';?>
 	<?php include 'include/mixp_functions.inc';?>
-
 	
 	<!-- Bootstrap
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -42,99 +33,18 @@ else{
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	 Bootstrap --> 
 	<link rel="stylesheet" href="include/styles.css">
+	<link rel="stylesheet" href="include/styles_login.css">
 
-	<style>
-        *,
-        *::before,
-        *::after {
-            box-sizing: border-box;
-            -webkit-box-sizing: border-box;
-        }
-        ._bodyxx{
-            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-            background-color: #f7f7ff;
-            padding: 10px;
-            margin: 0;
-        }
-        ._container{
-            max-width: 400px;
-            background-color: #ffffff;
-            padding: 2px;
-            margin: 0 auto;
-            border: 0px solid #cccccc;
-            border-radius: 1px;
-        }
-
-        ._img{
-            overflow: hidden;
-            width: 30px;
-            height: 30px;
-            margin: 0 auto;
-            border-radius: 50%;
-            box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
-			text-align: center;
-			
-			vertical-align: top;
-        }
-        ._img > img{
-            width: 30px;
-            min-height: 30px;
-        }
-        ._info{
-            text-align: center;
-			display: inline-block;
-			line-height: 50%;
-        }
-        ._info h1{
-            margin:10px 0;
-            text-transform: capitalize;
-        }
-        ._info p{
-            color: #555555;
-        }
-        ._info a{
-            display: inline-block;
-            background-color: #E53E3E;
-            color: #fff;
-            text-decoration: none;
-            padding:5px 10px;
-            border-radius: 2px;
-            border: 1px solid rgba(0, 0, 0, 0.1);
-        }
-
-		._login a{
-            display: inline-block;
-            background-color: #006600;
-            color: #fff;
-            text-decoration: none;
-            padding:5px 10px;
-            border-radius: 2px;
-            border: 1px solid rgba(0, 0, 0, 0.1);
-        }
-
-		#izquierda {
-float:left;
-}
-
-#derecha {
-float:left;
-width:200px;
-background:#fc0;
-}
-    </style>
-
-
-	
- 	<title>Buenos Aires Top - Las mejores opciones donde comer, donde divertirse, seguros de vehículos.</title>
+	<title>Buenos Aires Top - Las mejores opciones donde comer, donde divertirse, seguros de vehículos.</title>
 </head>
 
 <body>
 <br>
 <!-- encabezado-->
 <div class="container" >
-	<div class="template">
-		<div class="row">
-		<div style="float:left;width: 70%;">
+<div class="template">
+<div class="row">
+	<div style="float:left;width: 70%;">
 			<!--<div class="col-sm-12 col-md-12 col-lg-12" align="left" valign="center"> -->
 		  		<img src="images/app/utils/logo2.jpg"  alt="" longdesc="" id="img_logo"  height="56" width="117">
 			
@@ -143,38 +53,37 @@ background:#fc0;
 			<div class="col-sm-8 col-md-6 col-lg-8" valign="center" align="left">  -->
 				<!--<p class="h1"  align="left">Guía de Buenos Aires</p>--> 
 				<h1 align="left" style="color:#137791">Guía de Buenos Aires</h1>
-</div>
+	</div> <!-- float al 70% -->
 		<!--	</div>
 			<div class="col-sm-6 col-md-3 col-lg-2" valign="center" align="right">  -->
-			<?php
-				if(isset($_SESSION['login_id']) == TRUE){
+<?php
+if(isset($_SESSION['login_id']) == TRUE){
 //  if ($user['name'] != NULL) {
-					?>
-<div style="float:right;width: 30%;">
- 				<div class="_container">
-        			
-        			<div class="_info">
-					<div class="_img" align="center">
-            			<img src="<?php echo $user['profile_image']; ?>" alt="<?php echo $user['name']; ?>">
-        			</div>
-            			<p style style="line-height: 50%"><?php echo $user['name']; ?></p>
-            			<p style style="line-height: 50%"><?php echo $user['email']; ?></p>
-            			<a href="logout.php">Logout</a>
-        			</div>
-				</div>
-				</div>
-			<?php
- 			}
- 			else{ 
+?>
+	<div style="float:right;width: 30%;">
+ 		<div class="_container">       			
+        <div class="_info">
+			<div class="_img" align="center">
+            	<img src="<?php echo $user['profile_image']; ?>" alt="<?php echo $user['name']; ?>">
+        	</div>
+            <p style style="line-height: 50%"><?php echo $user['name']; ?></p>
+            <p style style="line-height: 50%"><?php echo $user['email']; ?></p>
+            <a href="logout.php">Logout</a>
+        </div> <!-- info -->
+		</div> <!-- container -->
+	</div> <!-- float al 30% -->
+<?php
+}
+else{ 
 	 
 	//require 'db_connection.php';
 
-	if(isset($_SESSION['login_id'])){
+	//if(isset($_SESSION['login_id'])){
 	//	header('Location: index.php');
 	//	exit;
-	}
+	//}
 	
-			require 'google_login/google-api/vendor/autoload.php';
+	require 'google_login/google-api/vendor/autoload.php';
 	
 			// Creating new google client instance
 	$client = new Google_Client();
@@ -188,31 +97,28 @@ background:#fc0;
 	
 	// Adding those scopes which we want to get (email & profile Information)
 	$client->addScope("email");
-	$client->addScope("profile");
-	 
-	 
-	 
-	 ?>
-			
-			<div align="right" style="float:left;width: 30%;">
-			<p style style="line-height: 100%">
-				<a href="<?php echo $client->createAuthUrl(); ?>">
-					<img src="images/app/utils/login-google.png"  alt="" longdesc="" id="img_logo" height="22" width="70">
-				</a><br>
-				 <!-- Button to Open the Modal -->
-  <button type="button" class="btn btn-link" data-toggle="modal" data-target="#myModal2" align="right">
-  <small align="right">¿Porque loguearme?</small>
-  </button></p>
-			</div>
+	$client->addScope("profile"); 
+	 ?>		
+	<div align="right" style="float:left;width: 30%;">
+		<p style style="line-height: 100%">
+		<a href="<?php echo $client->createAuthUrl(); ?>">
+		<img src="images/app/utils/login-google.png"  alt="" longdesc="" id="img_logo" height="22" width="70">
+		</a><br>
+		<!-- Button to Open the Modal -->
+  		<button type="button" class="btn btn-link" data-toggle="modal" data-target="#myModal2" align="right">
+  		<small align="right">¿Porque loguearme?</small>
+  		</button></p>
+	</div> <!-- float al 30% -->
 <?php 
 }
  ?>
- </div>
+ <!-- </div> -->
 
-		</div> <!--row-->
-	</div>
+</div> <!--row-->
+</div> <!-- template -->
 </div> <!--container-->
 
+<!-- link OneSignal que es el de push notifications -->
 <div class="container" align="right">
 <div class="template" align="right">
 <div class="row" align="right">
@@ -221,6 +127,8 @@ background:#fc0;
 		<div class='onesignal-customlink-container' align="right"></div>
 	</div>
 </div></div></div> 
+<!-- fin del link OneSignal que es el de push notifications -->
+
 <br>
  <!-- opciones del menu-->  
 <div class="container" >
@@ -258,17 +166,13 @@ background:#fc0;
 		   <a href="#" onclick="openPage('services.php');"><p class="h1">Serv. y productos</p>
 			<img src="images/app/menu/opt_servprof.jpeg" width="120" height="75" alt="Servicios y productos" longdesc="Servicios y productos" id="img_serv" /></a>
 			<p class="text_menu">Seguros, salud, estética, ...</p>
-		   <hr> 
-		   
-		   
-		</div>	
-
-		
+		   <hr>    
+		</div>			
 	</div> <!--div row -->
-	</div>  <!--div template -->
+</div>  <!--div template -->
 </div> <!-- class="container" -->
 
-<!-- GetButton.io widget -->
+<!-- BOTON WHATSAPP ME -->
 <script type="text/javascript">
 (function () {
 var options = {
@@ -294,14 +198,8 @@ $('#myModal').on('shown.bs.modal', function () {
 include 'include/footer.inc';
 ?>
 
-
-
-
-
-
+<!-- DIV PARA MOSTRAR POOPUP INFO DE PORQUE LOGUEARME -->
 <div class="container">
-  
-
   <!-- The Modal -->
   <div class="modal" id="myModal2">
     <div class="modal-dialog">
@@ -330,9 +228,8 @@ include 'include/footer.inc';
         
       </div>
     </div>
-  </div>
-  
+  </div> 
 </div>
-
+<!-- FIN DE DIV PARA MOSTRAR POOPUP INFO DE PORQUE LOGUEARME -->
 </body>
 </html>
