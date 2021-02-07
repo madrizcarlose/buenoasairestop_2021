75235c19af91905c06a252d3cbe46a2fca633cde
Carlos Madriz <madrizcarlose@gmail.com>
2021-02-07 16:05:40 -0300

    commit 02071605

--- listsites.php
+++ listsites.php
@@ -2,11 +2,11 @@
 
 $categoria = $_GET['categoria'];
 $latt = $_GET['lat'];
-print ($latt);
+//print ($latt);
 
 
 $lont = $_GET['lon'];
-print ($lont);
+//print ($lont);
 //$str_sql="SELECT N.nomb_negocio,N.codi_negocio , N.desc_negocio ,N.valoracion ,N.telef_negocio ,N.imagen_negocio ,N.direccion_negocio,N.coord_negocio, N.coord2, N.webpage FROM t_negocios N JOIN tr_negocios_categorias R ON N.codi_negocio = R.codi_negocio WHERE R.id_categoria ='" .$categoria. "' AND Activo=1;";
 
 //$str_sql="SELECT N.* FROM t_negocios N JOIN tr_negocios_categorias R ON N.codi_negocio = R.codi_negocio WHERE R.id_categoria ='" .$categoria. "' AND Activo=1;";
