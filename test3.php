d378ff1cb1454465e6be118928a1c2fbd8e0774e
Carlos Madriz <madrizcarlose@gmail.com>
2021-02-13 06:28:59 -0300

    commit 02120628

--- test3.php
+++ test3.php
@@ -4,7 +4,7 @@
 
 <body style="font-family:sans-serif; ">      
 <?php              
-$url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&fields=name,rating,formatted_phone_number&key=AIzaSyDG-R6WTvqjWqLb-01oG4LV7HV_az9Ajhw" 
+$url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&fields=name,rating,formatted_phone_number&key=AIzaSyDG-R6WTvqjWqLb-01oG4LV7HV_az9Ajhw" ;
 
 // $_GET['categoria'];
 $data = file_get_contents($url);
