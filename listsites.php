8264fdb9e72960a63e9f85a6c814156e88a87b13
Carlos Madriz <madrizcarlose@gmail.com>
2021-02-13 07:37:54 -0300

    commit 02130737

--- listsites.php
+++ listsites.php
@@ -50,6 +50,35 @@ $products = json_decode($data,true);
       padding: 0 20px;
     }
   </style>
+
+<style type="text/css">        
+    /* Rate Star*/        
+    .result-container{
+        width: 100px; height: 22px;
+        background-color: #ccc;
+        vertical-align:middle;
+        display:inline-block;
+        position: relative;
+        top: -4px;
+    }
+    .rate-stars{
+        width: 100px; height: 22px;
+        background: url(images/app/stars/stars.png) no-repeat;
+        background-size: cover;
+        position: absolute;
+    }
+    .rate-bg{
+        height: 22px;
+        background-color: #ffbe10;
+        position: absolute;
+    }
+    /* Rate Star Ends*/
+    
+    /* Display rate count */    
+    .reviewCount, .reviewScore {font-size: 14px; color: #666666; margin-left: 5px;}
+    .reviewScore {font-weight: 600;}
+    /* Display rate count Ends*/        
+    </style>
    <?php include 'include/head.inc';?>
    <!--
    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
@@ -165,6 +194,20 @@ for($i=0; $i<$longitud; $i++) {
         <div id="site2<?php echo $row['codi_negocio']; ?>">
             <h4><font color="#086A87"><?php print $row['nomb_negocio']; ?></h4></font>        
         </div>
+        <?php
+    //defining Product id
+    
+                $rate_times = 12;
+                $rate_value = 5;
+                $rate_bg = 66;
+           
+    ?>
+        <div style="margin-top: 10px">
+                <div class="result-container">
+                <div class="rate-bg" style="width:<?php echo $rate_bg; ?>%"></div>
+                <div class="rate-stars"></div>
+            </div>                    
+            <span class="reviewScore"><?php echo substr($rate_value,0,3); ?></span><span class="reviewCount">(<?php echo $rate_times; ?> reviews)</span>
         </h5>
        <p>
 		    <span class="glyphicon glyphicon-earphone"></span>
