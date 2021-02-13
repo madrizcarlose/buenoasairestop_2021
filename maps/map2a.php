beda60da2c6e97e032abf3db5a7ca79fd5252ac5
Carlos Madriz <madrizcarlose@gmail.com>
2021-02-13 10:57:04 -0300

    commit 02131057

--- maps/map2a.php
+++ maps/map2a.php
@@ -86,7 +86,7 @@ $var_sql="SELECT N.*, Round((( 3959 * acos( cos( radians('$latf') ) * cos( radia
 
   <div id="map" class="col-xs-12 col-sm-12 col-md-10 col-lg-10" ></div>
 <div id="list" class="col-xs-12 col-sm-12 col-md-2 col-lg-2" >
-<div style="width: 200px; height: 800px; overflow-y: scroll;">
+<div style="width: 100%; height: 800px; overflow-y: scroll;">
   <?php
  
  $parametros = "categoria=" . $categoria  . "&lat=".$lat . "&lon=".$lon;
