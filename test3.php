6c5dc24f7c20d930bda345b14d34df201514e9d6
Carlos Madriz <madrizcarlose@gmail.com>
2021-02-13 06:27:51 -0300

    commit 02130627

---
+++ test3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<body style="font-family:sans-serif; ">      
+<?php              
+$url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&fields=name,rating,formatted_phone_number&key=AIzaSyDG-R6WTvqjWqLb-01oG4LV7HV_az9Ajhw" 
+
+// $_GET['categoria'];
+$data = file_get_contents($url);
+$print ($data);
+?>
+</html>
